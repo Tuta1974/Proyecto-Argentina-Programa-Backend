52168368b6b3dfb1c293046b325f48247d04836a
Tuta1974 <comunica.arte@gmail.com>
2022-08-01 18:11:19 -0300

    configuración cross origin

--- src/main/java/com/porfolio/tuta/Controller/CSkills.java
+++ src/main/java/com/porfolio/tuta/Controller/CSkills.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/skills")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://porfolio-gustavo-a-romero.firebaseapp.com/")
 public class CSkills {
     @Autowired
     SkillService skillService;
