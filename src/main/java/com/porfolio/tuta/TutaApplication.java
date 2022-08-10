77a214db50fb40cd98f8d9a1bebb0694da6784d9
Tuta1974 <comunica.arte@gmail.com>
2022-08-10 09:36:06 -0300

    corrigiedo cors en el back

--- src/main/java/com/porfolio/tuta/TutaApplication.java
+++ src/main/java/com/porfolio/tuta/TutaApplication.java
@@ -17,32 +17,7 @@ public class TutaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TutaApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
 
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-
-				registry.addMapping("/**").allowedOrigins("https://porfolio-gustavo-a-romero.web.app");
-			}
-		};
-	}
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://porfolio-gustavo-a-romero.web.app"));
-
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);	}
 
 
 
